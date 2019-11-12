b2863468bcdc0d752447eb2d912d8894a09c3dd5
Austin Galey <S01131461@acad.tri-c.edu>
2019-11-11 22:07:18 -0500

    Singular IF statement added

---
+++ Lab 3 Singular If Statement/Lab 3 Singular If Statement/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab3_Singular_If_Statement 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int speedLimit;
+            int speed;
+
+            Console.WriteLine("Enter Speed:");
+
+            speedLimit = 35;
+            speed = Convert.ToInt32(Console.ReadLine());
+
+            if (speed > speedLimit)
+                Console.WriteLine("SLOW DOWN");
+
+        }
+    }
+}
