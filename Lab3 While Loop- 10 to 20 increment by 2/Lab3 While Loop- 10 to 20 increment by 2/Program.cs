fe586191ce4af5602367a3689614e91505d48d85
Austin Galey <S01131461@acad.tri-c.edu>
2019-11-17 22:38:05 -0500

    Entered while loop 10 to 20 increment

---
+++ Lab3 While Loop- 10 to 20 increment by 2/Lab3 While Loop- 10 to 20 increment by 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab3_While_Loop__10_to_20_increment_by_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            number = 10;
+            while (number <= 20)
+            {
+                Console.WriteLine(number);
+                number = number + 2;
+            }
+        }
+    }
+}
