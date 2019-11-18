bc7606c3ce346009df3760b84ef5ebb1fc8e4062
Austin Galey <S01131461@acad.tri-c.edu>
2019-11-17 22:19:30 -0500

    Entered 60- 20 loop. Decrement by 5

---
+++ Lab3  While Loop- 60 to 20 decrement by 5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab3__While_Loop__60_to_20_decrement_by_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            number = 60;
+            while (number >= 20)
+            {
+                Console.WriteLine(number);
+                number = number - 5;
+            }
+        }
+    }
+}
