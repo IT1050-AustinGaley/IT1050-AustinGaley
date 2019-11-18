131a79d45f73dba45b1caab5af9e8aa27a9940c4
Austin Galey <S01131461@acad.tri-c.edu>
2019-11-17 23:03:27 -0500

    Started Lab4

---
+++ Lab4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            i = 1;
+            while (i <= 100)
+            {
+                Console.WriteLine(i);
+            
+                i = i + 1;
+
+                if (i % 2 == 0)
+                    Console.WriteLine("\nit is odd");
+
+                else
+                    Console.WriteLine("\nit is even");
+                Console.WriteLine("*****");
+                // Question 1:
+                // Question 2:
+                // Question 3:
+
+            }
+        }
+    }
+}
