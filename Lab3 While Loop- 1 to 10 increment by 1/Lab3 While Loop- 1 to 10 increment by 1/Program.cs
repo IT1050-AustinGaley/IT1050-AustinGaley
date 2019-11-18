2ff7f68bd6b0914bbfbbc8ed7af8b2147c99d85b
Austin Galey <S01131461@acad.tri-c.edu>
2019-11-17 22:02:55 -0500

    Added "while" loop 0-10 increment by 1

---
+++ Lab3 While Loop- 1 to 10 increment by 1/Lab3 While Loop- 1 to 10 increment by 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab3_While_Loop__1_to_10_increment_by_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            number = 0;
+           while (number < 11)
+            {
+                Console.WriteLine(number);
+                number = number + 1;
+            }
+        }
+    }
+}
