3feeea66860977dc408d194684fed03d1693dac5
Austin Galey <S01131461@acad.tri-c.edu>
2019-11-17 19:54:45 -0500

    Added comment on similarties and differences between single-selection statements and while-repetition statements

--- Lab3/Lab3/Program.cs
+++ Lab3/Lab3/Program.cs
@@ -19,6 +19,7 @@ namespace Lab03
 
             else
                 Console.WriteLine("It is False!");
+            // for a single selection statement, the program takes two or more values and compares them. After comparing, the program then uses logic to determine the output. The same can be said for while repetition statements. One major difference between the two is that the while-repetition statement will loop until a certain condition is met and for the single-selection statement as the name suggest, there is only one value either compared or inputted then the program ends.
         }
     }
 }
