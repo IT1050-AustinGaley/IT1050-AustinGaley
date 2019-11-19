1cca8a69ede0c36f91081ef3121b8bcad52eedaa
Austin Galey <S01131461@acad.tri-c.edu>
2019-11-18 23:02:34 -0500

    Added code fix 0-100

---
+++ Lab4 0-100 correction/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab4_0_100_correction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 101; i++)
+                Console.WriteLine("{0}\n********", i);
+           
+
+        }
+    }
+}
+//for (int i = 0; i < 101; i++)
+//      Console.WriteLine(i);
+//      Console.WriteLine("********");
+//
+//  This code will only "WriteLine" the "********" at the end of the for process. The program will first increment by 1 till i is no longer less than 101. Once that completes, it then reads the next part to "WriteLine the *******"
+//
+//
