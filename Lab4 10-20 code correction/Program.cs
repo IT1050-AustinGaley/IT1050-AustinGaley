208a7109efb983bc6e1b4e6e1c47a41b8742b6e2
Austin Galey <S01131461@acad.tri-c.edu>
2019-11-18 22:56:38 -0500

    Added code correction 1

---
+++ Lab4 10-20 code correction/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab4_10_20_code_correction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 9;
+                while (i < 20)
+                 Console.WriteLine(i= i + 1);
+
+           //int i = 10;
+           //while (i < 21)
+           //{
+           //      Console.WriteLine(i);
+           // }
+           // this causes an infinite loop since "i" is not being incremented by 1 during the "while" process. This causes "i" to always be at the value 10 which is always less then 21 which causes the while loop to go forever
+
+
+        }
+    }
+}
