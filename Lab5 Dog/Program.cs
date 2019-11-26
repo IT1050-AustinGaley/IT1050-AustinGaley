82cbf5b997ff81aebbbd2438cb5efef0de0177f9
Austin Galey <S01131461@acad.tri-c.edu>
2019-11-25 23:47:19 -0500

    Part 2 Lab 5

---
+++ Lab5 Dog/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dog
+{
+    class Dog
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog();
+            myDog.name = "Fido";
+
+
+            // To do: uncomment the following lines of code to call the methods
+            myDog.bark();
+            Console.ReadLine();
+        }
+        private static void bark()
+        {
+            Console.WriteLine("{0} is Barking...");
+        }
+
+        private static void doTrick()
+        {
+            //trick name
+            Console.WriteLine("{0} is so smart! {0} is doing a(n) {1}",)
+        }
+    }
+}
