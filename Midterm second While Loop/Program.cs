b7a31707d21502bccbcb3ac0ed081aebe74170d9
Austin Galey <S01131461@acad.tri-c.edu>
2019-12-01 22:38:59 -0500

    Added second while loop-Midterm

---
+++ Midterm second While Loop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Midterm_second_While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            while (x < 128)
+                {
+                    x=x*2;
+                Console.Write("\n[{0}]", x);
+                }
+
+        }
+
+    }
+}
