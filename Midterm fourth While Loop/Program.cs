b15e8ca6594c9fc7d749b1a2e6e384d582720ca6
Austin Galey <S01131461@acad.tri-c.edu>
2019-12-01 22:49:28 -0500

    Added fourth while loop- Midterm

---
+++ Midterm fourth While Loop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Midterm_fourth_While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            while (x < 21)
+            {
+                x++;
+                if (x % 2 == 0)
+                    Console.Write("");
+                else
+                    Console.Write("{0} ", x);
+
+            }
+
+        }
+    }
+}
