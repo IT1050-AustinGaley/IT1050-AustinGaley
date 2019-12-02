fd2dd00c3fb2818946b62af45b82f364a4a86496
Austin Galey <S01131461@acad.tri-c.edu>
2019-12-01 22:42:58 -0500

    Added third while loop- Midterm

---
+++ Midterm third While Loop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Midterm_third_While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 50;
+            while (x > 1)
+            {
+                x--;
+                Console.Write("{0},", x);
+            }
+        }
+    }
+}
