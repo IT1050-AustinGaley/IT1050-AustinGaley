8cbc372c84ae21f65c13320b4675ec6cf0a4113d
Austin Galey <S01131461@acad.tri-c.edu>
2019-12-02 21:37:32 -0500

    Added Lets Go Outside!

---
+++ Midterm Let's Go Outside/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Midterm_Let_s_Go_Outside
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string icyRain;
+            string tornadoWarning;
+           
+
+            Console.WriteLine("Let's Go Outside!");
+            Console.WriteLine("What's the weather like? Is it icy?");
+            icyRain = Console.ReadLine();
+
+            if (icyRain == "yes")
+                Console.WriteLine("There's too much sleet to go out today");
+            else
+                Console.WriteLine("is it windy?");
+                tornadoWarning = Console.ReadLine();
+            if (tornadoWarning == "yes")
+                Console.WriteLine("There's a possible tornado warning for today, lets stay indoors");
+            else
+                Console.WriteLine("Time to go outside!");
+           
+
+             
+
+        }
+    }
+}
+
---
+++ Midterm output for given code/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Midterm_output_for_given_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 8;
+            int i = 10; // initialize
+            do
+            {
+                Console.Write("*");
+                i++; // update!
+            } while (i < n); // test condition
+
+        }
+    }
+}
+//  Output is a single "*" since this is a do-while
+// Output in a regular while statment would be "* *" since it would loop twice until i was no longer < n
